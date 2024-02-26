eb0b39766181c4fb674f49976ae4e851b1fb2dd3
chaotic <mnrealest161@gmail.com>
2024-02-26 19:14:34 +0700

    router  2051150161

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,7 +2,7 @@ const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
 
 module.exports.routesClient = (app) => {
-    app.get("/", homeRoutes);
+    app.use("/", homeRoutes);
     
     app.use("/products", productRoutes);
 }
