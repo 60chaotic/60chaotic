dc37cdac21acfc8a7fde310b912c4c70317affc9
chaotic <mnrealest161@gmail.com>
2024-02-26 22:14:43 +0700

    change

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -19,11 +19,11 @@ route.delete("/delete/:id", controller.deleteItem);
 
 route.get("/create", controller.create);
 
-route.post("/create", upload.single('thumbnail'), validate.createPost, controller.createPost);
+route.post("/create", upload.single('thumbnail'), controller.createPost);
 
 route.get("/edit/:id", controller.edit);
 
-route.patch("/edit/:id", upload.single('thumbnail'), validate.createPost, controller.editPatch);
+route.patch("/edit/:id", upload.single('thumbnail'), controller.editPatch);
 
 route.get("/detail/:id", controller.detail);
 
