81ee71404531b8ee60ad8866312af9e878a2cfc1
chaotic <mnrealest161@gmail.com>
2024-02-27 19:19:07 +0700

    update

--- validates/admin/product.validate.js
+++ validates/admin/product.validate.js
@@ -1,4 +1,4 @@
-module.exports.createPost = (req, res, next) => {
+module.exports.createPost = async (req, res, next) => {
     if(!req.body.title) {
         req.flash("error", "Tiêu đề không được để trống!")
         res.redirect("back");
